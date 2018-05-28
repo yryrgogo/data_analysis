50455ae92a93ce0bccdf2438a296bf6cad464f53
Go Horikoshi <go.horikoshi@gixo.jp>
2018-05-28 13:33:46 +0900

    some

--- module/convinience.py
+++ module/convinience.py
@@ -12,21 +12,22 @@ from select_feature import select_feature
 def move_feature():
 
     #  path_list = glob.glob('../features/3_winner/*.csv')
-    winner_list = select_feature()
 
     #  winner_list = []
     #  for path in path_list:
     #      filename = re.search(r'/([^/.]*).csv', path).group(1)
     #      winner_list.append(filename[2:])
 
-    path_list = glob.glob('../features/1_second_valid/*.npy')
+    winner_list = pd.read_csv('../output/use_feature/auc_0.7761917332858363_feature_importance.csv')['feature'].values
+    path_list = glob.glob('../features/1_first_valid/*.npy')
 
     for path in path_list:
         for win in winner_list:
             win = win.replace(' ', '_')
+            win = f'{win}.npy'
             if path.count(win):
                 print(path)
-                shutil.move(path, '../features/4_loser/')
+                shutil.move(path, '../features/3_winner/')
 
 
 
