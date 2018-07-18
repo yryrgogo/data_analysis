809af1b65465df62b92baab16e8b99f4c4eade98
Go Horikoshi <go.horikoshi@gixo.jp>
2018-07-18 22:53:27 +0900

    some

--- module/statistics_info.py
+++ module/statistics_info.py
@@ -11,5 +11,17 @@ def correlation(df):
 
     corr = df.corr(method='pearson')
 
+
     return corr
+
+
+def correlation_selection():
+
+    thres = 0.95
+
+    ' そのfeatureと相関がxx以上のfeatureを取り出す '
+    for feature in corr.columns:
+        coef_ary = corr[feature].values
+        coef_ary = coef_ary[coef_ary>thres]
+
 
