3522a5503d805c8f7efee5235b12beda4e302278
Go Horikoshi <go.horikoshi@gixo.jp>
2018-09-20 19:02:42 +0900

    some

---
+++ library/slack_api.py
@@ -0,0 +1,86 @@
+
+def sendMessage(channel_key='test', sendText='test'):
+    import urllib.parse
+    import urllib.request
+    import requests
+    import json
+    import sys
+    from xml.sax.saxutils import unescape   # エスケープされた記号を戻す
+
+    ' Slack token load'
+    token = ''
+
+    url = {'dm': "https://slack.com/api/im.history?",
+           'ch': "https://slack.com/api/channels.history?"
+           }
+
+    ' チャンネル '
+    channel = {'dm': "D6FJTP6HE",  # target channel id
+               'times_go': "C6G6K2L11",
+               'test': "G9Q8V5340"
+               }
+    '''
+    Explain:
+        指定されたチャンネルにメッセージを投稿する
+    Args:
+        channel_id (str)
+            送信対象のチャンネルのID文字列
+        sendText (str)
+            送信するメッセージの内容
+    Return:
+        None
+    '''
+
+    URL='https://slack.com/api/chat.postMessage'
+    url_ch = {'dm': "https://slack.com/api/im.history?",
+           'ch': "https://slack.com/api/channels.history?"
+           }
+    ' チャンネル '
+    channel = {'dm': "D6FJTP6HE",  # target channel id
+               'times_go': "C6G6K2L11",
+               'test': "G9Q8V5340"
+               }
+
+    channel_id = channel[channel_key]
+
+    params = {
+        'token': token,
+        'channel': channel_id,
+        'text': sendText,
+        'username': 'DataRobot_Bot',
+        #  "icon_emoji": u":datarobot:",
+        "icon_emoji": u":hori:",
+        'link_names': 1,  # 名前をリンク化
+    }
+
+    requests.post(URL, data=params)
+
+    return
+
+
+def create_params(channel, count=10):
+    params = {
+        'token':      token,
+        'channel':    channel,
+        'count':      count
+    }
+    return urllib.parse.urlencode(params)
+
+
+def get_messages():
+    res = urllib.request.urlopen(url['ch'] + params)
+    body = res.read().decode("utf-8")
+    response = json.loads(body)
+
+    for r in response['messages']:
+        if r['type'] == 'message':
+            print(unescape(r['text'].replace('\n', ' ')))
+
+
+if __name__ == '__main__':
+    #     params = create_params(channel=channel['times'], count=10)
+    #     get_messages()
+
+    message = 'hello'
+    ' チャンネルにメッセージを流す '
+    sendMessage('test', message)
