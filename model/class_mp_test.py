bb6118aeba330e8cff349de273138346d8c87354
Go Horikoshi <go.horikoshi@gixo.jp>
2018-10-22 07:35:23 +0900

    some

--- model/class_mp_test.py
+++ model/class_mp_test.py
@@ -13,6 +13,15 @@ class multi_test:
     def comment(self, comment):
         print(comment)
 
-    def pararell_comment(self, comment_list=[]):
-        pararell_process(self.comment, self.comment_list)
+    def multi_comment(self, c1, c2, c3):
+        print(c1)
+        print(c2)
+        print(c3)
+
+    def com_wrapper(self, args):
+        return self.multi_comment(*args)
+
+    def pararell_comment(self, c_list=[]):
+        #  pararell_process(self.comment, self.comment_list)
+        pararell_process(self.com_wrapper, c_list, cpu_cnt=2)
 
--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-from model.lightgbm_ex import lightgbm_ex as lgb_ex
+#  from model.lightgbm_ex import lightgbm_ex as lgb_ex
+from model.class_mp_test import multi_test
+
+c_list = [
+    ['aaa', 'ddd', 'fff']
+    ,['bbb', 'eee', 'hhh']
+    ,['ccc', 'ggg', 'aas']
+]
+
+mt = multi_test()
+mt.pararell_comment(c_list)
