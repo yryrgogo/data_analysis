48f210cf57881c7eec09d234e67cec494f1bd1a2
Go Horikoshi <go.horikoshi@gixo.jp>
2018-11-18 09:38:47 +0900

    some

--- library/utils.py
+++ library/utils.py
@@ -23,6 +23,7 @@ import pickle
 import gzip
 from multiprocessing import Pool
 import multiprocessing
+from contextlib import contextmanager
 
 from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
 
@@ -35,6 +36,18 @@ COMPETITION_NAME = 'home-credit-default-risk'
 # =============================================================================
 # def
 # =============================================================================
+@contextmanager
+def timer(name):
+    """
+    Taken from Konstantin Lopuhin https://www.kaggle.com/lopuhin
+    in script named : Mercari Golf: 0.3875 CV in 75 LOC, 1900 s
+    https://www.kaggle.com/lopuhin/mercari-golf-0-3875-cv-in-75-loc-1900-s
+    """
+    t0 = time.time()
+    yield
+    print(f'[{name}] done in {time.time() - t0:.0f} s')
+
+
 def start(fname):
     global st_time
     st_time = time()
